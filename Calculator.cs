d6996ca4cba6ec6ce406f24b23b766b6332c1b29
Moriz <william.dedominico72@k12.leanderisd.org>
2021-08-16 13:32:24 -0500

    fun donwe

---
+++ Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double total = 0;
+        public double firstNumber = 0;
+        public double secondNumber = 0;
+        public char operation = ' ';
+
+        public Calculator()
+        {
+        }
+
+        public char changeOperation(char op)
+        {
+            operation = op;
+            return op;
+        }
+
+        public bool calculate()
+        {
+            try
+            {
+                switch (operation)
+                {
+                    case '+':
+                        add();
+                        break;
+                    case '-' :
+                        subtract();
+                        break;
+                    case '*':
+                        multiply();
+                        break;
+                    case '/':
+                        divide();
+                        break;
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public double add()
+        {
+            total = firstNumber + secondNumber;
+            return total;
+        }
+
+        public double subtract()
+        {
+            total = firstNumber - secondNumber;
+            return total;
+        }
+
+        public double multiply()
+        {
+            total = firstNumber * secondNumber;
+            return total;
+        }
+
+        public double divide()
+        {
+            total = firstNumber / secondNumber;
+            return total;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,122 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private String numBuilder = "";
+        private Calculator calc = new Calculator();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numBuilder += 1;
+            label1.Text = numBuilder;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numBuilder += 2;
+            label1.Text = numBuilder;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numBuilder += 3;
+            label1.Text = numBuilder;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            numBuilder += 4;
+            label1.Text = numBuilder;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            numBuilder += 5;
+            label1.Text = numBuilder;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            numBuilder += 6;
+            label1.Text = numBuilder;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            numBuilder += 7;
+            label1.Text = numBuilder;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            numBuilder += 8;
+            label1.Text = numBuilder;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            numBuilder += 9;
+            label1.Text = numBuilder;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            calc.changeOperation('+');
+            calc.firstNumber = Int32.Parse(numBuilder);
+            numBuilder = "";
+            label1.Text = "Enter Second Number";
+            label2.Text = ""+calc.firstNumber;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            calc.changeOperation('-');
+            calc.firstNumber = Int32.Parse(numBuilder);
+            numBuilder = "";
+            label1.Text = "Enter Second Number";
+            label2.Text = ""+calc.firstNumber;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            calc.changeOperation('*');
+            calc.firstNumber = Int32.Parse(numBuilder);
+            numBuilder = "";
+            label1.Text = "Enter Second Number";
+            label2.Text = ""+calc.firstNumber;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            calc.changeOperation('/');
+            calc.firstNumber = Int32.Parse(numBuilder);
+            numBuilder = "";
+            label1.Text = "Enter Second Number";
+            label2.Text = ""+calc.firstNumber;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            numBuilder += 0;
+            label1.Text = numBuilder;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            calc.secondNumber = Int32.Parse(numBuilder);
+            calc.calculate();
+            numBuilder = "";
+            label1.Text = ""+calc.total;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
